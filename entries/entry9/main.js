ae3423c71f2444b74bb4817651cc90ed2961d2dc
zhuy007 <zhuy007@newschool.edu>
2023-11-19 17:52:31 -0500

    entry9

--- entries/entry9/main.js
+++ entries/entry9/main.js
@@ -1,2 +1,9 @@
 let body = document.querySelector(".intro");
 let button = document.querySelector(".button");
+
+function uncover() {
+  body.classList.toggle("off");
+  button.classList.toggle("text");
+}
+
+button.addEventListener("click", uncover);
